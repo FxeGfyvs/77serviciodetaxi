dfe92dcdf7e53cce484c86c32d9495428000dd32
FxeGfyvs <51398297+FxeGfyvs@users.noreply.github.com>
2019-11-05 00:16:21 +0800

    Add files via upload

---
+++ XtaxiTests/XtaxiTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import Xtaxi
+class XtaxiTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    override func tearDown() {
+        super.tearDown()
+    }
+    func testExample() {
+    }
+    func testPerformanceExample() {
+        self.measureBlock {
+        }
+    }
+}
---
+++ XtaxiUITests/XtaxiUITests.swift
@@ -0,0 +1,13 @@
+import XCTest
+class XtaxiUITests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    override func tearDown() {
+        super.tearDown()
+    }
+    func testExample() {
+    }
+}
